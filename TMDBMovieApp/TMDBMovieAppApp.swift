1b785561821ace7c45a5634bc819b444ef3f5887
Noufal <noufal.github@gmail.com>
2026-07-27 18:21:03 +0700

    Initial Commit

---
+++ TMDBMovieApp/TMDBMovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TMDBMovieAppApp.swift
+//  TMDBMovieApp
+//
+//  Created by Noufal Ibrahim on 27/07/26.
+//
+
+import SwiftUI
+
+@main
+struct TMDBMovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
